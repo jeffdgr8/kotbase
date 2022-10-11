017d1f1cbf09b2a5876185e49c881f76db411fcb
Jeff Lockhart <jeffdgr8@gmail.com>
2022-10-10 23:02:27 -0600

    Native support for KTX extensions

--- couchbase-lite-ktx/build.gradle.kts
+++ couchbase-lite-ktx/build.gradle.kts
@@ -1,5 +1,11 @@
 @file:Suppress("UNUSED_VARIABLE")
 
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest
+import org.jetbrains.kotlin.konan.target.Architecture
+import org.jetbrains.kotlin.konan.target.Family
+import org.jetbrains.kotlin.konan.target.KonanTarget
+
 plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
@@ -16,11 +22,23 @@ version = cblVersion
 kotlin {
     explicitApiWarning()
 
-    jvm()
     android {
         publishLibraryVariants("release")
     }
+    jvm()
     ios()
+    iosSimulatorArm64()
+    macosX64()
+    macosArm64()
+    linuxX64()
+    // TODO: kotlinx atomicfu, datetime, and coroutines don't support arm64 or armhf
+    //  https://github.com/Kotlin/kotlinx.atomicfu/pull/193
+    //  https://github.com/Kotlin/kotlinx-datetime/issues/75
+    //  https://github.com/Kotlin/kotlinx.coroutines/issues/855
+    //  https://github.com/square/okio/issues/1006
+    //linuxArm64()
+    //linuxArm32Hfp()
+    mingwX64()
 
     cocoapods {
         name = "CouchbaseLite-KMP-KTX"
@@ -44,6 +62,15 @@ kotlin {
         }
     }
 
+    targets.withType<KotlinNativeTarget> {
+        if (!konanTarget.family.isAppleFamily) {
+            val libraryPath = "${konanTarget.libcblitePath}/${konanTarget.libcbliteLib}"
+            if (konanTarget.family == Family.LINUX) {
+                binaries.getTest(DEBUG).linkerOpts += listOf("-L$libraryPath", "-lcblite", "-rpath", libraryPath)
+            }
+        }
+    }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -57,6 +84,7 @@ kotlin {
                 implementation(project(":couchbase-lite"))
             }
         }
+
         val jvmCommonTest by creating {
             dependsOn(commonTest)
         }
@@ -81,6 +109,39 @@ kotlin {
                 implementation("androidx.test:runner:1.4.0")
                 implementation("io.mockk:mockk-android:1.12.5")
             }
+        }
+
+        val nativeCommonTest by creating {
+            dependsOn(commonTest)
+        }
+        val iosTest by getting {
+            dependsOn(nativeCommonTest)
+        }
+        val iosSimulatorArm64Test by getting {
+            dependsOn(iosTest)
+        }
+        val macosX64Test by getting {
+            dependsOn(nativeCommonTest)
+        }
+        val macosArm64Test by getting {
+            dependsOn(nativeCommonTest)
+        }
+
+        val linuxX64Test by getting {
+            dependsOn(nativeCommonTest)
+        }
+        // TODO: use linux arm builds from https://github.com/danbrough/kotlinxtras/
+        //val linuxArm64Main by getting {
+        //    dependsOn(nativeCommonTest)
+        //}
+        //val linuxArm64Test by getting {
+        //    dependsOn(nativeCommonTest)
+        //}
+        //val linuxArm32HfpTest by getting {
+        //    dependsOn(nativeCommonTest)
+        //}
+        val mingwX64Test by getting {
+            dependsOn(nativeCommonTest)
         }
     }
 }
@@ -121,3 +182,82 @@ if (System.getProperty("os.name") == "Linux") {
         )
     }
 }
+
+tasks.withType<KotlinNativeTest> {
+    val dir = name.substring(0, name.lastIndex - 3)
+    dependsOn(
+        tasks.register<Copy>("copy${name.capitalize()}Resources") {
+            from("src/commonTest/resources")
+            into("build/bin/$dir/debugTest")
+        }
+    )
+    if (dir.isWindows) {
+        dependsOn(
+            tasks.register<Copy>("copyLibcbliteDll") {
+                from("${libcblitePath(dir.os, dir.arch)}/bin/cblite.dll")
+                into("build/bin/$dir/debugTest")
+            }
+        )
+    }
+}
+
+val KonanTarget.os: String
+    get() {
+        return when (family) {
+            Family.LINUX -> "linux"
+            Family.MINGW -> "windows"
+            else -> error("Unhandled native OS: $this")
+        }
+    }
+
+val KonanTarget.arch: String
+    get() {
+        return when (architecture) {
+            Architecture.X64 -> "x86_64"
+            Architecture.ARM64 -> "arm64"
+            Architecture.ARM32 -> "armhf"
+            else -> error("Unhandled native architecture: $this")
+        }
+    }
+
+val KonanTarget.libcblitePath: String
+    get() = libcblitePath(os, arch)
+
+val KonanTarget.libcbliteLib: String
+    get() {
+        return when (family) {
+            Family.LINUX -> when (architecture) {
+                Architecture.X64 -> "lib/x86_64-linux-gnu"
+                Architecture.ARM64 -> "lib/aarch64-linux-gnu"
+                Architecture.ARM32 -> "lib/arm-linux-gnueabihf"
+                else -> error("Unhandled native architecture: $arch")
+            }
+            Family.MINGW -> "lib"
+            else -> error("Unhandled native OS: $os")
+        }
+    }
+
+val String.isWindows: Boolean
+    get() = startsWith("mingw")
+
+val String.os: String
+    get() {
+        return when {
+            startsWith("linux") -> "linux"
+            startsWith("mingw") -> "windows"
+            else -> error("Unhandled native OS: $this")
+        }
+    }
+
+val String.arch: String
+    get() {
+        return when {
+            endsWith("X64") -> "x86_64"
+            endsWith("Arm64") -> "arm64"
+            endsWith("Arm32") -> "armhf"
+            else -> error("Unhandled native architecture: $this")
+        }
+    }
+
+fun libcblitePath(os: String, arch: String): String =
+    "${project(":couchbase-lite").projectDir}/libs/libcblite/$os/$arch/libcblite-$cblVersion"
