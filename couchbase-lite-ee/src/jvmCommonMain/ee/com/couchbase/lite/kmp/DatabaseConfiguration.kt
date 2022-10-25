5711160c4cf84a56081f6e4b2641342595d962c3
Jeff Lockhart <jeffdgr8@gmail.com>
2022-10-25 13:05:51 -0600

    Fix JvmName

--- couchbase-lite-ee/src/jvmCommonMain/ee/com/couchbase/lite/kmp/DatabaseConfiguration.kt
+++ couchbase-lite-ee/src/jvmCommonMain/ee/com/couchbase/lite/kmp/DatabaseConfiguration.kt
@@ -1,3 +1,5 @@
+@file:JvmName("DatabaseConfigurationJvm") // https://youtrack.jetbrains.com/issue/KT-21186
+
 package com.couchbase.lite.kmp
 
 public actual var DatabaseConfiguration.encryptionKey: EncryptionKey?
