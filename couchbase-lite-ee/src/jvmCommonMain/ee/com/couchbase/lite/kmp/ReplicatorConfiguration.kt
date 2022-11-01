7a2f2852225a1e294916ba71b951a0d28854815d
Jeff Lockhart <jeffdgr8@gmail.com>
2022-11-01 17:03:15 -0600

    Fix JvmName

--- couchbase-lite-ee/src/jvmCommonMain/ee/com/couchbase/lite/kmp/ReplicatorConfiguration.kt
+++ couchbase-lite-ee/src/jvmCommonMain/ee/com/couchbase/lite/kmp/ReplicatorConfiguration.kt
@@ -1,3 +1,5 @@
+@file:JvmName("ReplicatorConfigurationEEJvm") // https://youtrack.jetbrains.com/issue/KT-21186
+
 package com.couchbase.lite.kmp
 
 public actual var ReplicatorConfiguration.isAcceptOnlySelfSignedServerCertificate: Boolean
