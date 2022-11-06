725db99b3982872b84481b8a1a679a6b0580f65d
Jeff Lockhart <jeffdgr8@gmail.com>
2022-11-05 23:35:10 -0600

    Fix JvmName

--- couchbase-lite/src/jvmCommonMain/kotlin/com/couchbase/lite/kmp/CouchbaseLiteException.kt
+++ couchbase-lite/src/jvmCommonMain/kotlin/com/couchbase/lite/kmp/CouchbaseLiteException.kt
@@ -1,3 +1,5 @@
+@file:JvmName("CouchbaseLiteExceptionJvm") // https://youtrack.jetbrains.com/issue/KT-21186
+
 package com.couchbase.lite.kmp
 
 public actual typealias CouchbaseLiteException = com.couchbase.lite.CouchbaseLiteException
