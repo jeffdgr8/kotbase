896f5acd963e068d75f8800b562b38f8435e2254
Jeff Lockhart <jeffdgr8@gmail.com>
2023-04-05 11:03:16 -0600

    Update Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.8.10" apply false
-    id("org.jetbrains.dokka") version "1.7.20" apply false
+    id("org.jetbrains.dokka") version "1.8.10" apply false
     id("com.android.library") version "7.4.2" apply false
 }
 
