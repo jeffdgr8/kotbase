d439b11310ad27af7e8c451a4e3518d24ae56e9c
Jeff Lockhart <jeffdgr8@gmail.com>
2023-04-29 09:34:57 -0600

    Update to final Kotlin 1.8.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.8.21-369" apply false
+    kotlin("multiplatform") version "1.8.21" apply false
     id("org.jetbrains.dokka") version "1.8.10" apply false
     id("com.android.library") version "7.4.2" apply false
     id("com.louiscad.complete-kotlin") version "1.1.0"
@@ -10,7 +10,6 @@ allprojects {
         google()
         mavenCentral()
         mavenLocal()
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/")
     }
     group = property("GROUP") as String
     val cblVersion = rootProject.libs.versions.couchbase.lite.java.get()
