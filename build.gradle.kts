f929283ec7dd782449b2c703d0cfca6395cdb021
Jeff Lockhart <jeffdgr8@gmail.com>
2023-05-30 11:30:07 -0600

    Remove complete Kotlin plugin
    Causes build error on Windows: IOException: CreateProcess error=206, The filename or extension is too long, which requires deleting ~\.konan to fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.dokka)
-    alias(libs.plugins.complete.kotlin)
 }
 
 allprojects {
