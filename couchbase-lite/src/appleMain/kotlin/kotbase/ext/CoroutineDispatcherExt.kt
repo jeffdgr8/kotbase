9d1404f3ec5c60c022b9b69cb8b761a329e02ff9
Jeff Lockhart <jeffdgr8@gmail.com>
2023-07-08 16:04:59 -0600

    Return serial dispatch queue

--- couchbase-lite/src/appleMain/kotlin/kotbase/ext/CoroutineDispatcherExt.kt
+++ couchbase-lite/src/appleMain/kotlin/kotbase/ext/CoroutineDispatcherExt.kt
@@ -1,15 +1,18 @@
 package kotbase.ext
 
-import kotlinx.cinterop.convert
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import platform.darwin.DISPATCH_QUEUE_PRIORITY_DEFAULT
-import platform.darwin.dispatch_get_global_queue
+import platform.darwin.DISPATCH_QUEUE_SERIAL
 import platform.darwin.dispatch_get_main_queue
+import platform.darwin.dispatch_queue_attr_t
+import platform.darwin.dispatch_queue_create
 import platform.darwin.dispatch_queue_t
 
 internal fun CoroutineDispatcher.asDispatchQueue(): dispatch_queue_t =
     when (this) {
         Dispatchers.Main -> dispatch_get_main_queue()
-        else -> dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT.convert(), 0.convert())
+        else -> dispatch_queue_create(
+            "${toString()}.asDispatchQueue()",
+            DISPATCH_QUEUE_SERIAL as dispatch_queue_attr_t
+        )
     }
