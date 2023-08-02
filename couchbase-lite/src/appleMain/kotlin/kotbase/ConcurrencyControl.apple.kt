7db681586c4cc9b7fdf71f78c004cc0dfa4cb9c6
Jeff Lockhart <jeffdgr8@gmail.com>
2023-08-02 01:30:13 -0600

    Rename actual files with platform suffix

---
+++ couchbase-lite/src/appleMain/kotlin/kotbase/ConcurrencyControl.apple.kt
@@ -0,0 +1,25 @@
+package kotbase
+
+import cocoapods.CouchbaseLite.CBLConcurrencyControl
+import cocoapods.CouchbaseLite.CBLConcurrencyControl.kCBLConcurrencyControlFailOnConflict
+import cocoapods.CouchbaseLite.CBLConcurrencyControl.kCBLConcurrencyControlLastWriteWins
+
+public actual enum class ConcurrencyControl {
+    LAST_WRITE_WINS,
+    FAIL_ON_CONFLICT;
+
+    public val actual: CBLConcurrencyControl
+        get() = when (this) {
+            LAST_WRITE_WINS -> kCBLConcurrencyControlLastWriteWins
+            FAIL_ON_CONFLICT -> kCBLConcurrencyControlFailOnConflict
+        }
+
+    internal companion object {
+
+        internal fun from(value: CBLConcurrencyControl): ConcurrencyControl = when (value) {
+            kCBLConcurrencyControlLastWriteWins -> LAST_WRITE_WINS
+            kCBLConcurrencyControlFailOnConflict -> FAIL_ON_CONFLICT
+            else -> error("Unexpected CBLConcurrencyControl ($value)")
+        }
+    }
+}
