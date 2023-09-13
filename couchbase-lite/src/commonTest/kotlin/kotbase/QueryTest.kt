900a5a0380e6058d5aab6c4964a18369f013b4c0
Jeff Lockhart <jeffdgr8@gmail.com>
2023-09-12 21:02:56 -0600

    Increase create document delay
    Test sometimes fails in Kotlin 1.9.20-Beta

--- couchbase-lite/src/commonTest/kotlin/kotbase/QueryTest.kt
+++ couchbase-lite/src/commonTest/kotlin/kotbase/QueryTest.kt
@@ -1485,7 +1485,7 @@ class QueryTest : BaseQueryTest() {
         val token = query.addChangeListener(listener)
         try {
             // create one doc
-            executeAsync(600) {
+            executeAsync(700) {
                 try {
                     createNumberedDocInBaseTestDb(-1, 100)
                 } catch (e: CouchbaseLiteException) {
