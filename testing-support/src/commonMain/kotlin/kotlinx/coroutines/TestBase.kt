eaa7cb20bcbad715cc9db560ec65502206747cc7
Jeff Lockhart <jeffdgr8@gmail.com>
2023-09-15 19:45:33 -0600

    Add copyright comments

--- testing-support/src/commonMain/kotlin/kotlinx/coroutines/TestBase.kt
+++ testing-support/src/commonMain/kotlin/kotlinx/coroutines/TestBase.kt
@@ -1,5 +1,24 @@
-// adapted from https://github.com/Kotlin/kotlinx.coroutines/blob/042720589c6f438f77d84254bd2dceb569f01841/kotlinx-coroutines-core/common/test/TestBase.common.kt
-// and native https://github.com/Kotlin/kotlinx.coroutines/blob/042720589c6f438f77d84254bd2dceb569f01841/kotlinx-coroutines-core/native/test/TestBase.kt
+/*
+ * Copyright 2022-2023 Jeff Lockhart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Adapted from https://github.com/Kotlin/kotlinx.coroutines/blob/042720589c6f438f77d84254bd2dceb569f01841/kotlinx-coroutines-core/common/test/TestBase.common.kt
+ * and native https://github.com/Kotlin/kotlinx.coroutines/blob/042720589c6f438f77d84254bd2dceb569f01841/kotlinx-coroutines-core/native/test/TestBase.kt
+ */
+
 /*
  * Copyright 2016-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
