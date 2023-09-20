9bcfc2dc41e4509322710a5f49325351293d68f9
Jeff Lockhart <jeffdgr8@gmail.com>
2023-09-20 01:20:50 -0600

    Update AGP to 8.1.1 and Android SDK to 34

--- buildSrc/src/main/kotlin/base-convention.gradle.kts
+++ buildSrc/src/main/kotlin/base-convention.gradle.kts
@@ -57,7 +57,7 @@ tasks.withType<KotlinCompile> {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         minSdk = 22
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
