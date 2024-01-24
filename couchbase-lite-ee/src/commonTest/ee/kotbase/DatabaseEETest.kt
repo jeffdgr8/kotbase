44198d6877198efe9933ba4a9fe277db6f20fbd4
Jeff Lockhart <jeffdgr8@gmail.com>
2024-01-23 18:49:30 -0700

    Ignore deprecations in DatabaseEETest

--- couchbase-lite-ee/src/commonTest/ee/kotbase/DatabaseEETest.kt
+++ couchbase-lite-ee/src/commonTest/ee/kotbase/DatabaseEETest.kt
@@ -22,6 +22,7 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 import kotlin.time.Duration.Companion.seconds
 
+@Suppress("DEPRECATION")
 class DatabaseEETest : BaseReplicatorTest() {
 
     // DatabaseTest.swift
