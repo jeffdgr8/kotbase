cffe99fe09565d57871e23808f2fb2e705e0f83d
Jeff Lockhart <jeff@kotbase.dev>
2024-11-22 00:17:22 -0700

    Update Couchbase Lite to 3.2.1

---
+++ couchbase-lite-ee/vendor/libcblite/windows/x86_64/libcblite-3.2.1/include/cbl/CBL_Edition.h
@@ -0,0 +1,57 @@
+<<<<<<<< HEAD:couchbase-lite/vendor/libcblite/linux/arm64/libcblite-3.1.10/include/cbl/CBL_Edition.h
+//
+// CBL_Edition.h
+//
+// Copyright (c) 2018 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef COUCHBASE_ENTERPRISE
+/* #undef COUCHBASE_ENTERPRISE */
+#endif
+
+#define CBLITE_VERSION "3.1.10"
+#define CBLITE_VERSION_NUMBER 3001010
+#define CBLITE_BUILD_NUMBER 4
+#define CBLITE_SOURCE_ID "ec0eb21"
+#define CBLITE_BUILD_TIMESTAMP "2024-11-07T19:06:57Z"
+========
+//
+// CBL_Edition.h
+//
+// Copyright (c) 2018 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#ifndef COUCHBASE_ENTERPRISE
+#define COUCHBASE_ENTERPRISE
+#endif
+
+#define CBLITE_VERSION "3.2.1"
+#define CBLITE_VERSION_NUMBER 3002001
+#define CBLITE_BUILD_NUMBER 9
+#define CBLITE_SOURCE_ID "6728898+e322f9b"
+#define CBLITE_BUILD_TIMESTAMP "2024-10-30T14:18:50Z"
+>>>>>>>> ed58ae4fc (Update Couchbase Lite to 3.2.1):couchbase-lite-ee/vendor/libcblite/windows/x86_64/libcblite-3.2.1/include/cbl/CBL_Edition.h
