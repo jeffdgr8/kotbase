19db10c9a8da64eaa5417f5d3f636d21844e067c
Jeff Lockhart <jeff@kotbase.dev>
2024-11-23 09:04:00 -0700

    Remove no longer needed ExperimentalStdLibApi opt-ins

--- couchbase-lite/src/commonTest/kotlin/kotbase/ResultTest.kt
+++ couchbase-lite/src/commonTest/kotlin/kotbase/ResultTest.kt
@@ -22,7 +22,6 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 import kotlin.test.*
 
-@OptIn(ExperimentalStdlibApi::class)
 class ResultTest : BaseQueryTest() {
 
     @Test
