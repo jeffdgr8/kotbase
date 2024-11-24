ff8e65a175e08abe4cd7b4a8f009667eb51c37d5
Jeff Lockhart <jeff@kotbase.dev>
2024-11-24 11:42:13 -0700

    Update Defaults for 3.2 API

--- couchbase-lite/src/commonMain/kotlin/kotbase/Defaults.kt
+++ couchbase-lite/src/commonMain/kotlin/kotbase/Defaults.kt
@@ -16,67 +16,153 @@
 package kotbase
 
 public object Defaults {
-    public object LogFile {
-        /** Plaintext is not used, and instead binary encoding is used in log files  */
-        public const val USE_PLAIN_TEXT: Boolean = false
+    public object Database {
+        /**
+         * Full sync is off by default because the performance hit is seldom worth the benefit
+         */
+        public const val FULL_SYNC: Boolean = false
 
-        /** 512 KiB for the size of a log file  */
+        /**
+         * Memory mapped database files are enabled by default, except on MacOS
+         */
+        public const val MMAP_ENABLED: Boolean = true
+    }
+
+    public object LogFile {
+        /**
+         * Plaintext is not used, and instead binary encoding is used in log files
+         */
+        public const val USE_PLAINTEXT: Boolean = false
+        @Deprecated(
+            "Use USE_PLAINTEXT",
+            ReplaceWith("USE_PLAINTEXT")
+        )
+        public const val USE_PLAIN_TEXT: Boolean = USE_PLAINTEXT
+
+        /**
+         * 512 KiB for the size of a log file
+         */
         public const val MAX_SIZE: Long = 524288
 
-        /** 1 rotated file present (2 total, including the currently active log file)  */
+        /**
+         * 1 rotated file present (2 total, including the currently active log file)
+         */
         public const val MAX_ROTATE_COUNT: Int = 1
     }
 
-    public object Database {
-        /** Full sync is off by default  */
-        public const val FULL_SYNC: Boolean = false
-    }
-
     public object FullTextIndex {
-        /** Accents and ligatures are not ignored when indexing via full text search  */
+        /**
+         * Accents and ligatures are not ignored when indexing via full text search
+         */
         public const val IGNORE_ACCENTS: Boolean = false
     }
 
+    public object VectorIndex {
+        /**
+         * Vectors are not lazily indexed, by default
+         */
+        public const val IS_LAZY: Boolean = false
+
+        /**
+         * Vectors are encoded by using 8-bit Scalar Quantizer encoding, by default
+         */
+        public val ENCODING: VectorEncoding.ScalarQuantizerType = VectorEncoding.ScalarQuantizerType.SQ8
+
+        /**
+         * By default, vectors are compared using squared Euclidean metrics
+         */
+        public val DISTANCE_METRIC: VectorIndexConfiguration.DistanceMetric =
+            VectorIndexConfiguration.DistanceMetric.EUCLIDEAN_SQUARED
+
+        /**
+         * By default, the value will be determined based on the number of centroids, encoding types, and the
+         * encoding parameters.
+         */
+        public const val MIN_TRAINING_SIZE: Long = 0
+
+        /**
+         * By default, the value will be determined based on the number of centroids, encoding types, and the
+         * encoding parameters
+         */
+        public const val MAX_TRAINING_SIZE: Long = 0
+
+        /**
+         * By default, the value will be determined based on the number of centroids.
+         */
+        public const val NUM_PROBES: Long = 0
+    }
+
     public object Replicator {
-        /** Perform bidirectional replication  */
+        /**
+         * Perform bidirectional replication
+         */
         public val TYPE: ReplicatorType = ReplicatorType.PUSH_AND_PULL
 
-        /** One-shot replication is used, and will stop once all initial changes are processed  */
+        /**
+         * One-shot replication is used, and will stop once all initial changes are processed
+         */
         public const val CONTINUOUS: Boolean = false
 
-        /** A heartbeat messages is sent every 300 seconds to keep the connection alive  */
+        /**
+         * A heartbeat messages is sent every 300 seconds to keep the connection alive
+         */
         public const val HEARTBEAT: Int = 300
 
-        /** When replicator is not continuous, after 10 failed attempts give up on the replication  */
+        /**
+         * When replicator is not continuous, after 10 failed attempts give up on the replication
+         */
         public const val MAX_ATTEMPTS_SINGLE_SHOT: Int = 10
 
-        /** When replicator is continuous, never give up unless explicitly stopped  */
+        /**
+         * When replicator is continuous, never give up unless explicitly stopped
+         */
         public const val MAX_ATTEMPTS_CONTINUOUS: Int = Int.MAX_VALUE
 
-        /** Max wait time between retry attempts in seconds  */
-        public const val MAX_ATTEMPT_WAIT_TIME: Int = 300
+        /**
+         * Max wait time between retry attempts in seconds
+         */
+        public const val MAX_ATTEMPTS_WAIT_TIME: Int = 300
+        @Deprecated(
+            "Use MAX_ATTEMPTS_WAIT_TIME",
+            ReplaceWith("MAX_ATTEMPTS_WAIT_TIME")
+        )
+        public const val MAX_ATTEMPT_WAIT_TIME: Int = MAX_ATTEMPTS_WAIT_TIME
 
-        /** Purge documents when a user loses access  */
+        /**
+         * Purge documents when a user loses access
+         */
         public const val ENABLE_AUTO_PURGE: Boolean = true
 
-        /** Whether or not a replicator only accepts self-signed certificates from the remote  */
+        /**
+         * Whether or not a replicator only accepts self-signed certificates from the remote
+         */
         public const val SELF_SIGNED_CERTIFICATE_ONLY: Boolean = false
 
-        /** Whether or not a replicator only accepts cookies for the sender's parent domains  */
+        /**
+         * Whether or not a replicator only accepts cookies for the sender's parent domains
+         */
         public const val ACCEPT_PARENT_COOKIES: Boolean = false
     }
 
     public object Listener {
-        /** No port specified, the OS will assign one  */
+        /**
+         * No port specified, the OS will assign one
+         */
         public const val PORT: Int = 0
 
-        /** TLS is enabled on the connection  */
+        /**
+         * TLS is enabled on the connection
+         */
         public const val DISABLE_TLS: Boolean = false
 
-        /** The listener will allow database writes  */
+        /**
+         * The listener will allow database writes
+         */
         public const val READ_ONLY: Boolean = false
 
-        /** Delta sync is disabled for the listener  */
+        /**
+         * Delta sync is disabled for the listener
+         */
         public const val ENABLE_DELTA_SYNC: Boolean = false
     }
 }
