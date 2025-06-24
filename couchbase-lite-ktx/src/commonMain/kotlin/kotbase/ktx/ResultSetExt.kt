4632dd567c0af4135e9dee7ccb0bec438f259bfb
Jeff Lockhart <jeff@kotbase.dev>
2025-06-24 17:58:07 -0600

    Close ResultSet in countResult() convenience function

--- couchbase-lite-ktx/src/commonMain/kotlin/kotbase/ktx/ResultSetExt.kt
+++ couchbase-lite-ktx/src/commonMain/kotlin/kotbase/ktx/ResultSetExt.kt
@@ -71,4 +71,4 @@ public inline fun <T : Any> ResultSet.toObjects(factory: (String) -> T?): List<T
  * Read count result from [selectCount].
  */
 public inline fun ResultSet.countResult(): Long =
-    allResults().first().getLong(0)
+    use { rs -> rs.allResults().first().getLong(0) }
