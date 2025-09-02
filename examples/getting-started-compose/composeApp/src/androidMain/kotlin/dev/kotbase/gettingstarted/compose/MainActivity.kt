6ae14a448f79847b520a36fe529c2783484684ad
Jeff Lockhart <jeff@kotbase.dev>
2025-09-01 22:56:16 -0600

    Handle Android system bars and display cutout insets
    Keyboard capitalize sentences

--- examples/getting-started-compose/composeApp/src/androidMain/kotlin/dev/kotbase/gettingstarted/compose/MainActivity.kt
+++ examples/getting-started-compose/composeApp/src/androidMain/kotlin/dev/kotbase/gettingstarted/compose/MainActivity.kt
@@ -4,13 +4,18 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Box(Modifier.safeDrawingPadding()) {
+                App()
+            }
         }
     }
 }
