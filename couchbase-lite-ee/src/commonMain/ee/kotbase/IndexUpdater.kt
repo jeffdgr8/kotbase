ae5f26ba5bcd74aa9aff098e751eabadc69bd1af
Jeff Lockhart <jeff@kotbase.dev>
2025-09-16 13:05:12 -0600

    Add QueryIndex.beginUpdate and IndexUpdater

---
+++ couchbase-lite-ee/src/commonMain/ee/kotbase/IndexUpdater.kt
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2025 Jeff Lockhart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kotbase
+
+import kotlinx.datetime.Instant
+
+/**
+ * **ENTERPRISE EDITION API**
+ *
+ * `IndexUpdater` used for updating the index in lazy mode. Currently, the vector index
+ * is the only index type that can be updated lazily.
+ *
+ * **Note!!**
+ *
+ * The [finish] function implicitly closes the `IndexUpdater` if it succeeds.
+ * Use of a closed `IndexUpdater` or any container ([Dictionary] or [Array]) obtained
+ * from it will throw a [CouchbaseLiteError].
+ */
+public interface IndexUpdater : Iterable<Any?>, AutoCloseable {
+
+    /**
+     * The number of the items in the array.
+     */
+    public val count: Int
+
+    /**
+     * Gets value at the given index as an object. The object types are Blob,
+     * Array, Dictionary, Number, or String based on the underlying
+     * data type; or null if the value is null.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the Object or null.
+     */
+    public fun getValue(index: Int): Any?
+
+    /**
+     * Gets value at the given index as a String. Returns null if the value doesn't exist, or its value is not a String.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the String or null.
+     */
+    public fun getString(index: Int): String?
+
+    /**
+     * Gets value at the given index as a Number. Returns null if the value doesn't exist, or its value is not a Number.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the Number or nil.
+     */
+    public fun getNumber(index: Int): Number?
+
+    /**
+     * Gets value at the given index as an int.
+     * Floating point values will be rounded. The value `true` is returned as 1, `false` as 0.
+     * Returns 0 if the value doesn't exist or does not have a numeric value.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the int value.
+     */
+    public fun getInt(index: Int): Int
+
+    /**
+     * Gets value at the given index as a long.
+     * Floating point values will be rounded. The value `true` is returned as 1, `false` as 0.
+     * Returns 0 if the value doesn't exist or does not have a numeric value.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the long value.
+     */
+    public fun getLong(index: Int): Long
+
+    /**
+     * Gets value at the given index as a float.
+     * Integers will be converted to float. The value `true` is returned as 1.0, `false` as 0.0.
+     * Returns 0.0 if the value doesn't exist or does not have a numeric value.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the float value.
+     */
+    public fun getFloat(index: Int): Float
+
+    /**
+     * Gets value at the given index as a double.
+     * Integers will be converted to double. The value `true` is returned as 1.0, `false` as 0.0.
+     * Returns 0.0 if the property doesn't exist or does not have a numeric value.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the double value.
+     */
+    public fun getDouble(index: Int): Double
+
+    /**
+     * Gets value at the given index as a boolean.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the boolean value.
+     */
+    public fun getBoolean(index: Int): Boolean
+
+    /**
+     * Gets value at the given index as a Blob.
+     * Returns null if the value doesn't exist, or its value is not a Blob.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the Blob value or null.
+     */
+    public fun getBlob(index: Int): Blob?
+
+    /**
+     * Gets value at the given index as a Date.
+     * JSON does not directly support dates, so the actual property value must be a string, which is
+     * then parsed according to the ISO-8601 date format (the default used in JSON.)
+     * Returns null if the value doesn't exist, is not a string, or is not parsable as a date.
+     * NOTE: This is not a generic date parser! It only recognizes the ISO-8601 format, with or
+     * without milliseconds.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the Date value or null.
+     */
+    public fun getDate(index: Int): Instant?
+
+    /**
+     * Gets value at the given index as an Array.
+     * Returns null if the value doesn't exist, or its value is not an Array.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the Array object.
+     */
+    public fun getArray(index: Int): Array?
+
+    /**
+     * Gets a Dictionary at the given index. Return null if the value is not a dictionary.
+     *
+     * @param index the index. This value must not exceed the bounds of the array.
+     * @return the Dictionary object.
+     */
+    public fun getDictionary(index: Int): Dictionary?
+
+    /**
+     * Gets content of the current object as a List. The values contained in the returned
+     * List object are all JSON based values.
+     *
+     * @return the List object representing the content of the current object in the JSON format.
+     */
+    public fun toList(): List<Any?>
+
+    /**
+     * Encode an Array as a JSON string
+     *
+     * @return JSON encoded representation of the Array
+     * @throws CouchbaseLiteException on encoder failure.
+     */
+    public fun toJSON(): String
+
+    /**
+     * Sets the vector for the value corresponding to the index.
+     * Setting null value means that there is no vector for the value,
+     * and any existing vector will be removed when the finish() is
+     * called.
+     *
+     * @param value the vector.
+     * @param index the index at which to place the vector.
+     */
+    @Throws(CouchbaseLiteException::class)
+    public fun setVector(value: List<Float>?, index: Int)
+
+    /**
+     * Skip setting the vector for the value corresponding to
+     * the index. The vector will be required to compute and
+     * set again for the value when the QueryIndex's
+     * beginUpdate() is later called for updating the index.
+     */
+    public fun skipVector(index: Int)
+
+    /**
+     * Updates the index with the computed vectors and removes
+     * any index rows for which null vector was given.
+     * If there are any indexes that do not have their vector value
+     * set or are skipped, a [CouchbaseLiteException] will be thrown.
+     *
+     * Note: Before calling the finish() function, the
+     * set vectors are kept in the memory.
+     *
+     * @throws CouchbaseLiteException if there are any indexes that
+     * do not have their vector value set or are skipped.
+     */
+    @Throws(CouchbaseLiteException::class)
+    public fun finish()
+
+    /**
+     * Close the updater without a checked exception
+     */
+    override fun close()
+}
