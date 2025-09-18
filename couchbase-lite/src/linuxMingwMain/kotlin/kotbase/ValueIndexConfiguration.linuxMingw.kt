193444eb6fa70a231b1d02bf5be10171b6185f78
Jeff Lockhart <jeff@kotbase.dev>
2025-09-18 12:32:05 -0600

    Implement ValueIndexConfiguration.where for linuxMingw

--- couchbase-lite/src/linuxMingwMain/kotlin/kotbase/ValueIndexConfiguration.linuxMingw.kt
+++ couchbase-lite/src/linuxMingwMain/kotlin/kotbase/ValueIndexConfiguration.linuxMingw.kt
@@ -27,13 +27,23 @@ public actual constructor(expressions: List<String>) : IndexConfiguration(expres
 
     public actual constructor(vararg expressions: String) : this(expressions.toList())
 
+    public actual fun setWhere(where: String?): ValueIndexConfiguration {
+        this.where = where
+        return this
+    }
+
+    public actual var where: String? = null
+
     internal val actual: CValue<CBLValueIndexConfiguration>
         get() {
             val exp = expressions.joinToString(separator = ",")
+            val whr = where
             return cValue {
                 expressionLanguage = kCBLN1QLLanguage
                 expressions.buf = strdup(exp)
                 expressions.size = strlen(exp)
+                where.buf = whr?.let { strdup(whr) }
+                where.size = whr?.let { strlen(whr) } ?: 0U
             }
         }
 }
