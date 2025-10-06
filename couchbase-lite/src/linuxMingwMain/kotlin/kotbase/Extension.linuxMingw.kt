1fd298ee9c8002abfbc7551b2b59609d2481afd3
Jeff Lockhart <jeff@kotbase.dev>
2025-10-06 00:38:35 -0600

    Add Extension.enableVectorSearch()

---
+++ couchbase-lite/src/linuxMingwMain/kotlin/kotbase/Extension.linuxMingw.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025 Jeff Lockhart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package kotbase
+
+import kotbase.internal.fleece.toFLString
+import kotbase.internal.wrapCBLError
+import kotlinx.cinterop.memScoped
+import libcblite.CBL_EnableVectorSearch
+
+public actual object Extension {
+
+    public actual fun enableVectorSearch() {
+        wrapCBLError { error ->
+            memScoped {
+                // TODO: path to extension library
+                CBL_EnableVectorSearch("".toFLString(this), error)
+            }
+        }
+    }
+}
